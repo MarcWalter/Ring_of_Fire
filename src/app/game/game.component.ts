42a9fec3e40817e0e8bedec8ac35659e3f9baf54
Marc Walter <m.walter-dev@gmx.de>
2023-03-24 11:47:15 +0100

    new flip card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,21 +1,71 @@
 import { Component } from '@angular/core';
+import { OnInit } from "@angular/core";
+import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
   turnCard = false;
   cardTurned = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
   takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.turnAnimation();
+      this.resetAnimation();
+      this.changePlayer();
+    }
+  }
+
+  turnAnimation() {
     this.pickCardAnimation = true;
     setTimeout(() => {
       this.turnCard = true
     }, 500);
     setTimeout(() => {
       this.cardTurned = true;
-    }, 600);
+    }, 1000);
+  }
+
+  resetAnimation() {
+    setTimeout(() => {
+      this.pickCardAnimation = false;
+      this.turnCard = false;
+      this.cardTurned = false;
+      this.game.playedCards.push(this.currentCard);
+    }, 3000);
+  }
+
+  changePlayer() {
+    this.game.currentPlayer++;
+    this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
+    });
   }
 }
